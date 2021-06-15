b4668a79da0b8581ceb102eedbe4700fc468d345
Jorge Gamero <jorge_gd20@hotmail.com>
2021-06-15 18:47:21 -0500

    Primeras vistas

--- PC04/Controllers/HomeController.cs
+++ PC04/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using PC04.Data;
 using PC04.Models;
 
 namespace PC04.Controllers
@@ -12,21 +13,29 @@ namespace PC04.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
-
-        public IActionResult Index()
+        public IActionResult Index(Foto f)
         {
-            return View();
+            var fotos = _context.Fotos.OrderBy(f => f.Fecha).ToList();
+            return View(fotos);
         }
 
         public IActionResult Privacy()
         {
             return View();
         }
+
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
